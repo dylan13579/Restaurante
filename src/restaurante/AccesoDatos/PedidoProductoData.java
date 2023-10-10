650a4e3cf313588c8527532009ade7c0f25938f5
mauricio tolosa <tolosamauricio@hotmail.com>
2023-10-10 12:05:13 -0300

    subo pedidoProductodata

--- src/restaurante/AccesoDatos/PedidoProductoData.java
+++ src/restaurante/AccesoDatos/PedidoProductoData.java
@@ -1,7 +1,18 @@
 
 package restaurante.AccesoDatos;
 
+import java.sql.Connection;
+
 
 public class PedidoProductoData {
+    
+    private Connection wifi = null;
+    
+     public PedidoProductoData(){
+        
+        wifi = Conexion.getConexion();
+        
+    }
+
     
 }
