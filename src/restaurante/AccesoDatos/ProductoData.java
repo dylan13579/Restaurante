8615fe899378a65a477acb33d0667120d3c6f340
dylan13579 <jofrevillegasdylan@gmail.com>
2023-10-10 10:56:12 -0300

    mejoras en producto

---
+++ src/restaurante/AccesoDatos/ProductoData.java
@@ -0,0 +1,125 @@
+
+package restaurante.AccesoDatos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import restaurante.Entidades.Producto;
+
+
+public class ProductoData {
+    
+    
+    private Connection wifi = null;
+    
+    
+    public ProductoData(){
+        
+        wifi = Conexion.getConexion();
+        
+    }
+    
+    
+    public void guardarProducto(Producto producto){
+        
+        String sql = "INSERT INTO producto(codigoProducto, nombreProducto, descripcion, stock, precio, estado) VALUES (?, ?, ?, ?, ?, ?)";
+        
+        
+        try {
+            PreparedStatement ps = wifi.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, producto.getCodigoProducto());
+            ps.setString(2, producto.getNombreProducto());
+            ps.setString(3, producto.getDescripcion());
+            ps.setInt(4, producto.getStock());
+            ps.setDouble(5, producto.getPrecio());
+            ps.setBoolean(6, producto.isEstado());
+            
+            int mercaderia = ps.executeUpdate();
+            
+            if(mercaderia > 0){
+               ResultSet rs = ps.getGeneratedKeys();
+               if(rs.next()){
+                   int genera = rs.getInt(1);
+                   producto.setCodigoProducto(genera);
+                   
+                   JOptionPane.showMessageDialog(null, "Producto agreddo");
+               }
+            }
+           
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error intentando acceder a la tabla Producto");
+        }
+           
+    }  
+    
+    
+    public void modificarProducto(Producto producto){
+        
+        String sql = "UPDATE producto SET nombreProducto = ?,descripcion = ?,stock = ?,precio = ?,estado = ? WHERE codigoProducto = ?";
+        
+        try {
+            PreparedStatement ps = wifi.prepareStatement(sql);
+            ps.setString(1, producto.getNombreProducto());
+            ps.setString(2, producto.getDescripcion());
+            ps.setInt(3, producto.getStock());
+            ps.setDouble(4, producto.getPrecio());
+            ps.setBoolean(5, producto.isEstado());
+            ps.setInt(6, producto.getCodigoProducto());
+            
+            int exito = ps.executeUpdate();
+            
+            if(exito == 1){
+                    JOptionPane.showMessageDialog(null, "Se ha modificado el producto");
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error intentando acceder a la tabla Producto");
+        }
+
+    }
+    
+    public Producto buscarProductos(int codigoProducto){
+     
+        String sql = "SELECT idProducto, codigoProducto, nombreProducto, descripcion, stock, precio FROM producto WHERE codigoProducto = ? AND estado = 1";
+        
+        Producto producto = null;
+        
+        try {
+            PreparedStatement ps = wifi.prepareStatement(sql);
+            
+            ps.setInt(1, codigoProducto);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+                producto = new Producto();
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setCodigoProducto(rs.getInt("codigoProducto"));
+                producto.setDescripcion(rs.getString("nombreProducto"));
+                producto.setNombreProducto(rs.getString("descripcion"));
+                producto.setStock(rs.getInt("stock"));
+                producto.setPrecio(rs.getDouble("precio"));
+                producto.setEstado(true);
+                
+                
+            }else{
+                 JOptionPane.showMessageDialog(null, "El codigo de ese producto no existe");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error intentando acceder a la tabla Producto");
+        }
+        
+        return producto;
+    }
+         
+            
+}
--- src/restaurante/Restaurante.java
+++ src/restaurante/Restaurante.java
@@ -56,11 +56,16 @@ public class Restaurante {
 
 
         //Pruebas de Pedido
-        
+//        
 //        PedidoData pe = new PedidoData();
-//        //    
-//        //  Mesa mesa = new Mesa();
-//        //  mesa.setNumeroMesa(3);
+//   
+//          //Mesa mesa = new Mesa();
+//          //mesa.setNumeroMesa(1);
+//          
+//          Pedido pedido = new Pedido();
+//          pedido.setNombreMesero("Dylan");
+          
+          
 //    
 //            Pedido pedido = new Pedido();
 //            pedido.setIdPedido(7);
@@ -72,18 +77,42 @@ public class Restaurante {
 //
 //            //pe.guardarPedido(pedido, mesa);
 //            pe.modificarPedido(pedido);
-        
+              // pe.sumarPedido(mesa);
+             //pe.sumarPedidoPorMesero(pedido);
      
         //metodo cancelar
 //        PedidoData pe = new PedidoData();
-//        
+        
 //        pe.cancelarPedidoId(8);
 //        pe.cancelarPedidoNumeroMesa(9);
+         
     
-    
-    
-    
-    
+        //Pruebas de Producto
+        
+        //ProductoData produc = new ProductoData();
+//       Producto pro = new Producto(6,"Pizza","Calabresa",46,3645.55,true); //guardar un producto
+          //Producto pro = new Producto(6,"Empanadas","jamon y queso",100,2600,true); //modiifcar producto
+//        produc.guardarProducto(pro);
+          //produc.modificarProducto(pro);
+          
+         ProductoData pro = new ProductoData();
+         Producto buscar = pro.buscarProductos(5);
+         
+         if(buscar != null){
+             System.out.println("codigo = "+buscar.getCodigoProducto());
+             System.out.println("Nombre del Producto = "+buscar.getNombreProducto());
+             System.out.println("Descripcion = "+buscar.getDescripcion());
+             System.out.println("Stock = "+buscar.getStock());
+             System.out.println("Precio = "+buscar.getPrecio());
+             
+             
+             
+         }
+         
+
+
+
+
     }
              
     
