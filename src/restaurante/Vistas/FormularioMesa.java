09a10a9b13057ba4609cb720ba48c80831c3a659
dylan13579 <jofrevillegasdylan@gmail.com>
2023-10-14 12:20:40 -0300

    cambios

--- src/restaurante/Vistas/FormularioMesa.java
+++ src/restaurante/Vistas/FormularioMesa.java
@@ -38,7 +38,7 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
         jrbActivaMesa = new javax.swing.JRadioButton();
         jbNuevaMesa = new javax.swing.JButton();
         jbBorrarMesa = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        jbSalir = new javax.swing.JButton();
         jbModificarMesa = new javax.swing.JButton();
         jtDescripMesa = new javax.swing.JTextField();
         jbGuardarMesa = new javax.swing.JButton();
@@ -75,10 +75,10 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
             }
         });
 
-        jButton4.setText("Salir");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        jbSalir.setText("Salir");
+        jbSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                jbSalirActionPerformed(evt);
             }
         });
 
@@ -118,7 +118,7 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
                                 .addGap(18, 18, 18)
                                 .addComponent(jbBorrarMesa, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addComponent(jbSalir, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel2)
@@ -156,7 +156,7 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbNuevaMesa)
                     .addComponent(jbBorrarMesa)
-                    .addComponent(jButton4)
+                    .addComponent(jbSalir)
                     .addComponent(jbModificarMesa)
                     .addComponent(jbGuardarMesa))
                 .addGap(0, 56, Short.MAX_VALUE))
@@ -165,7 +165,7 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void jbSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbSalirActionPerformed
         // TODO add your handling code here:
         int eleccion = JOptionPane.showConfirmDialog(this, "Realmente desea salir?", "Confirmacion", JOptionPane.YES_NO_OPTION);
             if (eleccion== JOptionPane.YES_OPTION){
@@ -173,7 +173,7 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
             }else if (eleccion== JOptionPane.NO_OPTION){
                 JOptionPane.showConfirmDialog(null, "Desea seguir trabajando?", "Confirmacion", JOptionPane.CLOSED_OPTION);
             }
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_jbSalirActionPerformed
 
     private void jbBuscarMesaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbBuscarMesaActionPerformed
         // TODO add your handling code here:
@@ -314,7 +314,6 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -324,6 +323,7 @@ public class FormularioMesa extends javax.swing.JInternalFrame {
     private javax.swing.JButton jbGuardarMesa;
     private javax.swing.JButton jbModificarMesa;
     private javax.swing.JButton jbNuevaMesa;
+    private javax.swing.JButton jbSalir;
     private javax.swing.JRadioButton jrbActivaMesa;
     private javax.swing.JTextField jtDescripMesa;
     private javax.swing.JTextField jtNroMesa;
